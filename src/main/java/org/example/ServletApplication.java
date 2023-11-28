ec0c658530d505666a2f4cfbe8aa45c0b25e074b
王睿 <wangrui1066@geostar.com.cn>
2023-11-28 16:29:15 +0800

    init

---
+++ src/main/java/org/example/ServletApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletApplication.class, args);
+    }
+
+}
